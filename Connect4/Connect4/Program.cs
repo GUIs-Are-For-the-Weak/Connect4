3176ee6ae8361d02b4d508be74dbe9a1b61dd651
BreD1810 <32489229+BreD1810@users.noreply.github.com>
2018-02-08 23:57:45 +0000

    Updated Program.cs Now determines the number of players, and instantiates a game.

--- Connect4/Connect4/Program.cs
+++ Connect4/Connect4/Program.cs
@@ -6,9 +6,30 @@ namespace Connect4
     {
         static void Main(string[] args)
         {
-            Board board = new Board();
-            board.Draw();
-            Console.ReadLine();
+            Game game = new Game(DeterminePlayers(), 4); //2 players, 4 in a row to win
+            Console.ReadLine(); //Needed to stop the console window from closing
+        }
+
+        //Determine the number of players that are in the game
+        //TODO: Determine which players are AI and human.
+        private static int DeterminePlayers()
+        {
+            Console.WriteLine("How many players do you want?");
+            int answer = 0;
+            while (answer <= 1)
+            {
+                //Determine if the input is an integer, and store it in answer if it is.
+                if (!(Int32.TryParse(Console.ReadLine(), out answer)))
+                {
+                    Console.WriteLine("Please enter an integer.");
+                }
+                else if(answer <= 1)
+                {
+                    Console.WriteLine("Please enter a number above 1.");
+                }
+            }
+            Console.WriteLine("You have " + answer + " players in the game.");
+            return answer;
         }
     }
 }
