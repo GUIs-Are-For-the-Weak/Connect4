5b28fc1dfc0e30cdd59e6e654eb1c3f4f926abf4
BreD1810 <32489229+BreD1810@users.noreply.github.com>
2018-02-08 23:57:23 +0000

    Updated Game.cs Now takes input for number of players, game type and then sets up the board.

--- Connect4/Connect4/Game.cs
+++ Connect4/Connect4/Game.cs
@@ -2,5 +2,24 @@ using System;
 
 class Game
 {
-    
+
+    private int _numberOfPlayers; //Number of players in the game TODO: Determine if AI or Human
+    private int _gameType; //Number in a row needed for a win
+
+    //Constructor
+    public Game(int numberOfPlayers, int rowsToWin)
+    {
+        _numberOfPlayers = numberOfPlayers;
+        _gameType = rowsToWin;
+        SetupBoard();
+    }
+
+    //Start the game
+    public void SetupBoard()
+    {
+        //Create the board and display it on the screen
+        Board board = new Board();
+        board.Draw();
+    }
+
 }
