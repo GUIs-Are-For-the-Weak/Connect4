ba6e93fa3fb5b36e0b4a473519c18a8d25e33024
Bradley Garrod <32489229+BreD1810@users.noreply.github.com>
2018-02-09 18:03:05 +0000

    Updated Program.cs
    Updated game repeat loop.

--- Connect4/Connect4/Program.cs
+++ Connect4/Connect4/Program.cs
@@ -7,16 +7,33 @@ namespace Connect4
         static void Main(string[] args)
         {
             //Game repeat loop
-            Console.Clear();
             bool loop = true;
             do
             {
+                Console.Clear();
                 Game game = new Game();
                 Console.WriteLine("Do you want to play another game? Y/N");
-                if(Console.ReadLine().ToLower() == "n")
+                bool check = true;
+                do
                 {
-                    loop = false;
-                }
+                    string answer = Console.ReadLine();
+                    if (answer == "n")
+                    {
+                        Console.WriteLine("Press enter to close this window...");
+                        loop = false;
+                        check = false;
+                    } else if (answer == "y")
+                    {
+                        Console.WriteLine("Starting a new game...");
+                        System.Threading.Thread.Sleep(2000); //2 second wait time
+                        //Console.Clear();
+                        check = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter Y or N.");
+                    }
+                } while (check);
             } while (loop);
 
             Console.ReadLine(); //Needed to stop the console window from closing
