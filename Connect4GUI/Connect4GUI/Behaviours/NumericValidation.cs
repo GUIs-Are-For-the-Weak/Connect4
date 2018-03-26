d7e10201fb66a23c1424fbf88be8d7bd943f47a8
BreD1810 <32489229+BreD1810@users.noreply.github.com>
2018-03-26 17:57:20 +0100

    Created DependencyProperty for Validation...

---
+++ Connect4GUI/Connect4GUI/Behaviours/NumericValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Connect4GUI.Behaviours;
+
+namespace Connect4GUI.Behaviours
+{
+    public static class NumericValidation
+    {
+        public static readonly DependencyProperty ValidationType = DependencyProperty.RegisterAttached("ValidationType", typeof(ValidationMethod), typeof(TextBox), new PropertyMetadata(ValidationMethod.None, OnValidationTypeChanged));
+
+        public static void OnValidationTypeChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            //TODO: Attach event handler for Property Changed!!!!
+        }
+    }
+}
---
+++ Connect4GUI/Connect4GUI/Behaviours/ValidationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4GUI.Behaviours
+{
+    public enum ValidationMethod
+    {
+        None,
+        PositiveInteger
+    };
+}
