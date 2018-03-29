bb9a7258ca7bac56bdfde5fd69110b26d9222dea
BreD1810 <32489229+BreD1810@users.noreply.github.com>
2018-03-29 21:23:43 +0100

    VALIDATION!!!!!!!!!!!!!!!!!!!!!!!!

--- Connect4GUI/Connect4GUI/Behaviours/NumericValidation.cs
+++ Connect4GUI/Connect4GUI/Behaviours/NumericValidation.cs
@@ -1,21 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Connect4GUI.Behaviours;
 
 namespace Connect4GUI.Behaviours
 {
     public static class NumericValidation
     {
-        public static readonly DependencyProperty ValidationType = DependencyProperty.RegisterAttached("ValidationType", typeof(ValidationMethod), typeof(TextBox), new PropertyMetadata(ValidationMethod.None, OnValidationTypeChanged));
+        public static readonly DependencyProperty ValidationTypeProperty = DependencyProperty.RegisterAttached("ValidationType",
+            typeof(ValidationMethod), typeof(TextBox), new PropertyMetadata(ValidationMethod.None, OnValidationTypeChanged));
+
+        private static readonly Dictionary<TextBox, string> CurrentTextValues = new Dictionary<TextBox, string>();
 
         public static void OnValidationTypeChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
-            //TODO: Attach event handler for Property Changed!!!!
+            if (!(sender is TextBox b) || CurrentTextValues.ContainsKey(b))
+                return;
+
+            b.TextChanging += OnTextChanging;
+            CurrentTextValues[b] = b.Text;
+        }
+
+        private static void OnTextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            int selectionStart = sender.SelectionStart - 1;
+
+            foreach (char c in sender.Text)
+            {
+                switch (GetValidationType(sender))
+                {
+                    case ValidationMethod.None:
+                        break;
+                    case ValidationMethod.PositiveInteger:
+                        if (c > '9' || c < '0')
+                        {
+                            sender.Text = CurrentTextValues[sender];
+                            sender.SelectionStart = selectionStart;
+                            return;
+                        }
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(ValidationMethod), "Invalid ValidationMethod");
+                }
+            }
+            CurrentTextValues[sender] = sender.Text;
+        }
+
+        public static void SetValidationType(DependencyObject dp, ValidationMethod value)
+        {
+            dp.SetValue(ValidationTypeProperty, value);
+        }
+
+        public static ValidationMethod GetValidationType(DependencyObject dp)
+        {
+            return (ValidationMethod)dp.GetValue(ValidationTypeProperty);
         }
     }
 }
--- Connect4GUI/Connect4GUI/Behaviours/ValidationType.cs
+++ Connect4GUI/Connect4GUI/Behaviours/ValidationType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Connect4GUI.Behaviours
+﻿namespace Connect4GUI.Behaviours
 {
     public enum ValidationMethod
     {
